ce7a65b6add78202bee1eff63e3e1b2dae527b29
marco <marcocala92@gmail.com>
2021-03-19 15:55:07 +0100

    Init

---
+++ todo-list/src/test/java/org/marco/calamai/todolist/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package org.marco.calamai.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
