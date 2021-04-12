9a6ba6cb209a09fd680f700491c3cbd42123fbf8
marco <marcocala92@gmail.com>
2021-04-12 09:38:07 +0200

    Add find all ordered by done and deadline.

--- todo-list/src/main/java/org/marco/calamai/todolist/repositories/mongo/ToDoMongoRepository.java
+++ todo-list/src/main/java/org/marco/calamai/todolist/repositories/mongo/ToDoMongoRepository.java
@@ -1,9 +1,11 @@
 package org.marco.calamai.todolist.repositories.mongo;
 import java.math.BigInteger;
+import java.util.List;
 
 import org.marco.calamai.todolist.model.ToDo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ToDoMongoRepository extends MongoRepository<ToDo, BigInteger> {
-	
+
+	List<ToDo> findByOrderByDoneAscDeadlineAsc();
 }
