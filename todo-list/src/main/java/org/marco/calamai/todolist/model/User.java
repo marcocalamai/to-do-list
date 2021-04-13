88e9c29a78c930b4772ee647b1e6fc1ca9a7b2a5
marco <marcocala92@gmail.com>
2021-04-13 10:41:57 +0200

    Refactor User for implements UserDetails.

--- todo-list/src/main/java/org/marco/calamai/todolist/model/User.java
+++ todo-list/src/main/java/org/marco/calamai/todolist/model/User.java
@@ -1,18 +1,28 @@
 package org.marco.calamai.todolist.model;
 
 import java.math.BigInteger;
+import java.util.Collection;
 
-public class User {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails {
 	
+
+	private static final long serialVersionUID = 1L;
 	private BigInteger id;
 	private String username;
 	private String psw;
+	private String[] authorities;
 	
-	public User(String username, String psw) {
+	
+	public User(String username, String psw, String[] authorities) {
 		this.username = username;
 		this.psw = psw;
+		this.authorities = authorities;
 	}
-	
+
 	public BigInteger getId() {
 		return id;
 	}
@@ -20,9 +30,68 @@ public class User {
 	public String getUsername() {
 		return username;
 	}
-
-	public String getPsw() {
+	
+	@Override
+	public String getPassword() {
 		return psw;
 	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return AuthorityUtils.createAuthorityList(authorities);
+	}
+
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((psw == null) ? 0 : psw.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (psw == null) {
+			if (other.psw != null)
+				return false;
+		} else if (!psw.equals(other.psw))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	
 	
 }
