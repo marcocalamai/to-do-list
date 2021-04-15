9bb0b2bfb2518dd995860812cddbe3add9586395
marco <marcocala92@gmail.com>
2021-04-15 22:02:09 +0200

    Added code and html file to make the test pass.
    https://github.com/marcocalamai/to-do-list/issues/10

--- todo-list/src/main/java/org/marco/calamai/todolist/webcontrollers/LoginWebController.java
+++ todo-list/src/main/java/org/marco/calamai/todolist/webcontrollers/LoginWebController.java
@@ -1,5 +1,14 @@
 package org.marco.calamai.todolist.webcontrollers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginWebController {
+	
+	@GetMapping("/login")
+	public String getLoginIndex() {
+		return "loginPage";
+	}
 
 }
