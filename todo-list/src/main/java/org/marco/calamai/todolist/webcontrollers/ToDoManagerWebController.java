5c5556d521faba9ab1c3e525be56f781f327df81
marco <marcocala92@gmail.com>
2021-04-16 16:37:56 +0200

    Written code to make the test pass.
    https://github.com/marcocalamai/to-do-list/issues/12

--- todo-list/src/main/java/org/marco/calamai/todolist/webcontrollers/ToDoManagerWebController.java
+++ todo-list/src/main/java/org/marco/calamai/todolist/webcontrollers/ToDoManagerWebController.java
@@ -1,14 +1,31 @@
 package org.marco.calamai.todolist.webcontrollers;
 
+import java.util.List;
+
+import org.marco.calamai.todolist.model.ToDo;
+import org.marco.calamai.todolist.services.ToDoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ToDoManagerWebController {
 	
+	
+	private ToDoService toDoService;
+	
+	@Autowired
+	public ToDoManagerWebController(ToDoService toDoService) {
+		this.toDoService = toDoService;
+	}
+	
 	@GetMapping("/toDoManager")
-	public String getToDoModelIndex() {
-		return "toDoManagerPage";
+	public ModelAndView getToDoModelIndex(ModelAndView mav) {
+		List<ToDo> allToDo = toDoService.getAllToDoOrderByDoneAscDeadlineAsc();
+		mav.setViewName("toDoManagerPage");
+		mav.addObject("allToDo", allToDo);
+		return mav;
 	}
 	
 
