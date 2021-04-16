71c294a322e03b7d419a342ad9e7e2fdb2862eee
marco <marcocala92@gmail.com>
2021-04-16 16:37:19 +0200

    Test show all to do.
    https://github.com/marcocalamai/to-do-list/issues/12

--- todo-list/src/test/java/org/marco/calamai/todolist/webcontrollers/ToDoManagerWebControllerTest.java
+++ todo-list/src/test/java/org/marco/calamai/todolist/webcontrollers/ToDoManagerWebControllerTest.java
@@ -1,14 +1,25 @@
 package org.marco.calamai.todolist.webcontrollers;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.marco.calamai.todolist.model.ToDo;
+import org.marco.calamai.todolist.services.ToDoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,10 +33,29 @@ class ToDoManagerWebControllerTest {
 	@Autowired
 	private MockMvc mvc;
 	
+	@MockBean
+	private ToDoService toDoService;
+	
 	@Test @DisplayName("Check get ToDoManager status code and view name")
 	@WithMockUser(username = "userTest", password = "passwordTest", roles = "USER")
 	void testStatus200AndReturnToDoManagerView() throws Exception{
 		mvc.perform(get("/toDoManager")).andExpect(status().isOk()).andExpect(view().name("toDoManagerPage"));
+	}
+	
+	@Test @DisplayName("Test ToDoManager return list of all todo")
+	@WithMockUser(username = "userTest", password = "passwordTest", roles = "USER")
+	void testToDoManagerViewShowAllToDo() throws Exception {
+		ToDo toDo1 = new ToDo("username_1", "title_1", "description_1", LocalDate.now());
+		List<ToDo> allToDo = new ArrayList<>();
+		allToDo.add(toDo1);
+		
+		when(toDoService.getAllToDoOrderByDoneAscDeadlineAsc()).thenReturn(allToDo);
+		mvc.perform(get("/toDoManager"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("toDoManagerPage"))
+				.andExpect(model().attribute("allToDo", allToDo));
+		
+		verify(toDoService, times(1)).getAllToDoOrderByDoneAscDeadlineAsc();
 	}
 	
 
