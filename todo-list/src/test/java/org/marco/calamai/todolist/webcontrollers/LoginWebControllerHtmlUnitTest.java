6e7170a36f2437987762ce53969e42508be17806
marco <marcocala92@gmail.com>
2021-04-19 19:53:47 +0200

    Added HtmlUnit test for LoginWebController.
    Fixed view name in RegistrationWebController.
    
    https://github.com/marcocalamai/to-do-list/issues/14

--- todo-list/src/test/java/org/marco/calamai/todolist/webcontrollers/LoginWebControllerHtmlUnitTest.java
+++ todo-list/src/test/java/org/marco/calamai/todolist/webcontrollers/LoginWebControllerHtmlUnitTest.java
@@ -1,15 +1,24 @@
 package org.marco.calamai.todolist.webcontrollers;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.marco.calamai.todolist.services.ToDoService;
+import org.marco.calamai.todolist.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 @ExtendWith(SpringExtension.class)
@@ -19,6 +28,13 @@ class LoginWebControllerHtmlUnitTest {
 	@Autowired 
 	private WebClient webClient;
 	
+	@MockBean
+	private UserService userService;
+	
+	@MockBean
+	private ToDoService toDoService;
+	
+	
 	@Test @DisplayName("Test loginPage title")
 	void testHomePageTitle() throws Exception {
 		HtmlPage page = webClient.getPage("/login");
@@ -26,9 +42,24 @@ class LoginWebControllerHtmlUnitTest {
 	}
 	
 	@Test @DisplayName("Test loginPage should provide a link for index page")
-	void testLoginPageShouldProvideAButtonForIndexPage() throws Exception {
+	void testLoginPageShouldProvideALinkForIndexPage() throws Exception {
 	HtmlPage page = this.webClient.getPage("/login");
 	assertEquals("/", page.getAnchorByText("Go Back").getHrefAttribute());
+	assertThat(page.getBody().getTextContent()).doesNotContain("Wrong username or password!");
+	}
+	
+	@Test @DisplayName("Test loginPage when user not found should show error")
+	void testLoginPageWhenItFailShouldShowError() throws Exception {
+	webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);	
+	when(userService.loadUserByUsername("WrongUsername")).thenThrow(UsernameNotFoundException.class);
+	HtmlPage page = this.webClient.getPage("/login");
+	HtmlForm form = page.getFormByName("loginForm");
+	form.getInputByName("username").setValueAttribute("WrongUsername");
+	form.getInputByName("password").setValueAttribute("password_1");
+	page = form.getButtonByName("btn_submit").click();
+	
+	assertThat(page.getBody().getTextContent()).contains("Wrong username or password!");
+	verify(userService, times(1)).loadUserByUsername("WrongUsername");	
 	}
 
 }
