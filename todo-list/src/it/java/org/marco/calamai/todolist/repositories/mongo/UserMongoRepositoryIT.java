afccbc7ea1414c9c33257066b27570ff7ebd20dc
marco <marcocala92@gmail.com>
2021-04-26 18:34:50 +0200

    New IT for find User by username.
    https://github.com/marcocalamai/to-do-list/issues/16

--- todo-list/src/it/java/org/marco/calamai/todolist/repositories/mongo/UserMongoRepositoryIT.java
+++ todo-list/src/it/java/org/marco/calamai/todolist/repositories/mongo/UserMongoRepositoryIT.java
@@ -1,9 +1,11 @@
 package org.marco.calamai.todolist.repositories.mongo;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -46,6 +48,15 @@ class UserMongoRepositoryIT {
     	List<User> result = mongoOps.findAll(User.class);
     	assertThat(result).containsExactly(saved);
     	assertNotNull(result.get(0).getId()); 
+    }
+    
+    @Test @DisplayName("Find User by username")
+    void testFindbyUsername(){
+    	User user1 = new User("username", "password");
+    	User saved = mongoOps.save(user1);
+    	Optional<User> result = repository.findByUsername("username");
+    	assertThat(result).isPresent();
+    	assertEquals(saved, result.get());
     }
 
 }
